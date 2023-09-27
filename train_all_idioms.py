ccbf198ba7ea01627dcab121067b92c4bc75003c
Konstantinos <kvelenis@gmail.com>
2023-09-27 11:53:12 +0300

    First commit

--- train_all_idioms.py
+++ train_all_idioms.py
@@ -22,14 +22,17 @@ mainFolder = cwd + '/training_data/'
 logging = True
 
 for f in os.walk(mainFolder):
-    if f[2][0].endswith('.xml'):
-        print('f: ', f[0].split('/')[-1])
-        # check if logging is required
-        if logging:
-            training_log_file = cwd+'/training_logs/'+f[0].split('/')[-1]
-            prt.initialise_log_file( training_log_file )
-        idiom = tic.TrainingIdiom(f[0]+'/', logging=logging, log_file=training_log_file)
-        idiom = grp.group_gcts_of_idiom(idiom)
-        # save learned idiom
-        with open('trained_idioms/'+idiom.name+'.pickle', 'wb') as handle:
-            pickle.dump(idiom, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    print(f)
+    if len(f[2]) > 0 :
+
+        if f[2][0].endswith('.xml'):
+            print('f: ', f[0].split('/')[-1])
+            # check if logging is required
+            if logging:
+                training_log_file = cwd+'/training_logs/'+f[0].split('/')[-1]
+                prt.initialise_log_file( training_log_file )
+            idiom = tic.TrainingIdiom(f[0]+'/', logging=logging, log_file=training_log_file)
+            idiom = grp.group_gcts_of_idiom(idiom)
+            # save learned idiom
+            with open('trained_idioms/'+idiom.name+'.pickle', 'wb') as handle:
+                pickle.dump(idiom, handle, protocol=pickle.HIGHEST_PROTOCOL)
